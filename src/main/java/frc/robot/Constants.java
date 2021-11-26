fc97bb72cc8435395f2f42b3373f7d59c4a84de8
SidKam11 <kamath.siddarth@gmail.com>
2021-11-25 16:55:39 -0800

    Change of Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,12 +15,12 @@ package frc.robot;
 public final class Constants {
     public static final int RollerPort = 6;
 	public static final boolean RollerInvert = false;
-	public static final double IntakeSpeed = -0.4;
-	public static final double OuttakeSpeed = 0.5;
+	public static final double IntakeSpeed = -0.2;
+	public static final double OuttakeSpeed = 0.3;
 	
 	public static final int ArmPort = 5;
 	public static final boolean ArmInvert = true;
-	public static double liftArmSpeed = 0.15;
+	public static double liftArmSpeed = 0.1;
 	public static final double lowerArmSpeed = -0.05;
 
 	public static final int LEFT_FRONT = 3;
@@ -29,8 +29,8 @@ public final class Constants {
     public static final int RIGHT_BACK = 2;
 	public static final int XBOX_LEFT_Y_AXIS = 1;
 	public static final int XBOX_LEFT_X_AXIS = 0;
-	public static final double DRIVETRAINSPEED = 0.7;
-	public static final double DRIVE_FORWARD_TIME = 3.0;
-	public static final double AUTONOMOUS_SPEED = 0.4;
+	public static final double DRIVETRAINSPEED = 0.3;
+	public static final double DRIVE_FORWARD_TIME = 0.0;
+	public static final double AUTONOMOUS_SPEED = 0.0;
 	public static final int JOYSTICK_NUMBER = 0;
 }
